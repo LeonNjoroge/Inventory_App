fb3aa22d04c84caf4ce66ffe1d1425c772f4191b
LeonNjoroge <leonmuraguri@gmail.com>
2024-11-28 11:38:51 +0300

    Update front end dashboard

--- Front-End/src/app/(components)/Sidebar/index.tsx
+++ Front-End/src/app/(components)/Sidebar/index.tsx
@@ -1,8 +1,37 @@
+"use client";
+
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      {/* Top Logo */}
+      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+        <div>Logo</div>
+        <h1 className='font-extrabold text-2xl'>Inventory</h1>
+      
+      <button 
+        className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' 
+        onClick={()=>{}}
+        >
+          <Menu className='w-4 h-4' />
+      </button>
+      </div>
+
+      {/* Links */}
+      <div className='flex-grow mt-8'>
+
+        {/* Links here */}
+
+      </div>
+
+      {/* Footer */}
+      <div>
+        <p className='text-center text-xs text-grey-500'>&copy; 2024 Inventory App</p>
+      </div>
+
+    </div>
   )
 }
 
