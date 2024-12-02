7df0543115658b41ae177507db64761c580bbdd5
LeonNjoroge <leonmuraguri@gmail.com>
2024-12-02 20:36:09 +0300

    Rename folders

--- web-client/src/app/(components)/Sidebar/index.tsx
+++ web-client/src/app/(components)/Sidebar/index.tsx
@@ -69,8 +69,8 @@ const Sidebar = () => {
       <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
         isSidebarCollapsed ? "px-5":"px-8"
         }`}>
-        <div>Logo</div>
-        <h1 className={`${isSidebarCollapsed? "hidden" : "block"} font-extrabold text-2xl`}>Inventory</h1>
+        <div>logo</div>
+        <h1 className={`${isSidebarCollapsed? "hidden" : "block"} font-extrabold text-2xl`}>InventoVault</h1>
       
       <button 
         className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' 
--- web-client/src/app/layout.tsx
+++ web-client/src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Inventory App",
+  title: "InventoVault",
   description: "App to manage inventory for businesses",
 };
 
